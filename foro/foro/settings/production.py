ab5d37793dc7d0343d856c7273e41bc644e1cdea
John Doe <johndoe@example.com>
2021-11-30 14:23:48 -0300

    primer commit

---
+++ foro/foro/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['midominio.com.ar']
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    #BASE DE DATOS DE PRODUCCION
+}
